7d2308fbce6c818bd2581c0beba5f45bc3bcddc6
chenkai <chenkai@seemmo.com>
2020-04-03 11:38:58 +0800

    fix packages

--- packages/IPython/utils/pickleutil.py
+++
@@ -1,5 +0,0 @@
-from warnings import warn
-
-warn("IPython.tools.pickleutil has moved to ipykernel.pickleutil", stacklevel=2)
-
-from ipykernel.pickleutil import *
--- packages/IPython/utils/traitlets.py
+++
@@ -1,6 +0,0 @@
-
-from warnings import warn
-
-warn("IPython.tools.traitlets has moved to a top-level traitlets package.", stacklevel=2)
-
-from traitlets import *
